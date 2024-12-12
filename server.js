283a50dc901b8f09ca8ad8acc0275a4f23665c82
Lucas Nascimento <147888693+Lucas-Nascimentto@users.noreply.github.com>
2024-12-12 18:10:11 -0300

    consertando erro de cors

--- server.js
+++ server.js
@@ -5,15 +5,27 @@ import auth from './middlewares/auth.js';
 import cors from 'cors'
 
 const app = express();
-const corsOptions = {
-    origin: 'https://luxury-daffodil-bfe639.netlify.app', // Permitir apenas essa origem
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-    credentials: true, // Caso precise enviar cookies ou headers como 'Authorization'
-};
 
 app.use(express.json());
-app.use(cors(corsOptions));
+
+// Criar o middleware para permitir requisição externa
+app.use((req, res, next) => {
+    
+    // Qualquer endereço pode fazer requisição "*"
+    res.header("Access-Control-Allow-Origin", "*");
+    
+    // Tipos de método que a API aceita
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    
+    // Permitir o envio de dados para API
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    
+    // Executar o cors
+    app.use(cors());
+
+    next();
+});
+
 
 app.use('/', publicRoutes);
 app.use('/', auth, privateRoutes);
